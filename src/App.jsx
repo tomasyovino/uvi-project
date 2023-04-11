ecbf7ddb8f5f38bc91153c10cb34b4b513686489
tomasyovino <tomasyovino@gmail.com>
2023-04-11 09:22:11 -0300

    Display initial structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+
+  return (
+    <h1 className="font-bold">
+      Hello world!
+    </h1>
+  );
+};
+
+export default App;
---
+++ src/themes.js
@@ -0,0 +1,11 @@
+export const themes = {
+    backgrounds: {
+        primary: "#282C4B",
+        secondary: "#3D235A",
+        tertiary: "#460082"
+    },
+    colors: {
+        main: "#FFFFFF",
+        secondary: "#F07922"
+    }
+};
